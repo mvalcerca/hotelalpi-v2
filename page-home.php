6183551737549c0d96da89c7e40ddcd94e9d98c7
Marco Valcerca <m.valcerca@travelwebj.com>
2015-11-25 12:27:12 +0100

    Rimosso sottitolo serzione blog

--- page-home.php
+++ page-home.php
@@ -189,7 +189,7 @@
 
         <div class="row">
           <div class="col-md-12 text-center title-padding">
-            <h2>LIVE FROM ROMA <br><small><?php _e('News, eventi e promozioni da Hotel Alpi Roma'); ?></small></h2>
+            <h2>LIVE FROM ROMA <br><small></small></h2>
           </div>
         </div>
 
